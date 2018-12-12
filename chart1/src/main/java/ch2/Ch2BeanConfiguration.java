ac428106526147f530b62b2397d11469e4ca3016
baoxing <312904694@qq.com>
2018-12-12 16:43:15 +0800

    ch 2

--- chart1/src/main/java/ch2/Ch2BeanConfiguration.java
+++ chart1/src/main/java/ch2/Ch2BeanConfiguration.java
@@ -1,15 +1,19 @@
 package ch2;
 
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 public class Ch2BeanConfiguration {
 
-    @Bean
+    @Bean(autowire = Autowire.BY_NAME)
     public AccountService accountService() {
         AccountServiceImpl bean = new AccountServiceImpl();
-        bean.setAccountDao(accountDao());
+//        bean.setAccountDao(accountDao());
         return bean;
     }
 
@@ -23,6 +27,27 @@ public class Ch2BeanConfiguration {
     public AccountDao accountDaoJdbc() {
         AccountDaoJdbcImpl bean = new AccountDaoJdbcImpl();
         return bean;
+    }
+
+    @Bean
+    @Profile("dev")
+    public Foo devFoo(@Value("${name}") String name) {
+        Foo foo = new Foo();
+        foo.setName("dev " + name);
+        return foo;
+    }
+
+    @Bean
+    @Profile("prod")
+    public Foo prodFoo(@Value("${name}") String name) {
+        Foo foo = new Foo();
+        foo.setName("prod " + name);
+        return foo;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
     }
 
 }
--- chart1/src/main/java/ch2/Main.java
+++ chart1/src/main/java/ch2/Main.java
@@ -1,12 +1,34 @@
 package ch2;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
+
+import java.util.Collections;
 
 public class Main {
 
     public static void main(String[] args) {
 //        test1();
-        test2();
+//        test2();
+        test3();
+    }
+
+    private static void test3() {
+        AnnotationConfigApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext();
+        applicationContext.register(Ch2BeanConfiguration.class);
+
+        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        environment.setActiveProfiles("dev");
+        MutablePropertySources propertySources = environment.getPropertySources();
+        propertySources.addLast(new MapPropertySource("mapSource",
+                Collections.singletonMap("name", (Object) "my foo")));
+        applicationContext.refresh();
+
+        Foo foo = applicationContext.getBean(Foo.class);
+        System.out.println(foo.getName());
     }
 
     private static void test2() {
